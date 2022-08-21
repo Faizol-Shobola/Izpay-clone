ea8d831a039daa47179810301cfc24c03175c05e
Shobola Faizol <shobolafaizol@gmail.com>
2022-08-21 04:51:09 +0100

    Added section 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,23 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    colors: {
-      'white': '#FFFFFF',
-      'gray': '#D4D4D4',
-      'dark': '#151518',
-      'purple': '#8176FF',
-      'dark-purple': '#6B5EFF'
-    },
     extend: {
+      colors: {
+        'white': '#FFFFFF',
+        'gray': '#D4D4D4',
+        'dark': '#151518',
+        'purple': '#8176FF',
+        'dark-purple': '#6B5EFF'
+      },
+
       fontFamily: {
         head: ['Montserrat', 'sans-serif'],
         body: ['Work Sans', 'sans-erif'],
       },
+
+      backgroundImage: {
+        'section-four': "url('https://templatekit.jegtheme.com/izpay/wp-content/uploads/sites/30/2021/01/shopping-girl-home1@2x.jpg')"
+       }
     },
   },
   plugins: [],
