098e858769284b0ba2565d24ff2488edd73a58bb
Shobola Faizol <shobolafaizol@gmail.com>
2022-08-21 21:43:23 +0100

    Make home page responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,8 @@ module.exports = {
       backgroundImage: {
         'section-four': "url('https://templatekit.jegtheme.com/izpay/wp-content/uploads/sites/30/2021/01/shopping-girl-home1@2x.jpg')",
         'section-five': "url('https://templatekit.jegtheme.com/izpay/wp-content/uploads/sites/30/2020/12/bg-halftone@2x.png')",
-        'section-six': "url('https://templatekit.jegtheme.com/izpay/wp-content/uploads/sites/30/2021/01/testi-bg@2x.png')"
+        'section-six': "url('https://templatekit.jegtheme.com/izpay/wp-content/uploads/sites/30/2021/01/testi-bg@2x.png')",
+        'section-seven': "url('https://templatekit.jegtheme.com/izpay/wp-content/uploads/sites/30/2021/01/payment-img@2x.jpg')",
        }
     },
   },
