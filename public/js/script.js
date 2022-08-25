72bd2a737fee36e0c38498afeb01bb67d43f6ce5
shobola faruq <shobolafaruq@gmail.com>
2022-08-25 02:04:34 +0100

    Make the stats count on about-us page

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,43 @@
+new numberRush('number-one', {
+    speed: 30,
+    steps: 1,
+})
+
+new numberRush('number-two', {
+    speed: 85,
+    steps: 1,
+})
+
+new numberRush('number-three', {
+    speed: 85,
+    steps: 1,
+})
+
+new numberRush('number-four', {
+    speed: 65,
+    steps: 1,
+})
+
+new numberRush('number-five', {
+    speed: 30,
+    steps: 1,
+})
+
+new numberRush('number-six', {
+    speed: 35,
+    steps: 1,
+})
+
+new numberRush('number-seven', {
+    speed: 35,
+    steps: 1,
+})
+
+new numberRush('number-eight', {
+    speed: 35,
+    steps: 1,
+})
+
 var openMenu = document.querySelector(".open_menu");
 var closeMenu = document.querySelector(".close_menu");
 var mobileNavigation = document.querySelector(".mobile-menu");
